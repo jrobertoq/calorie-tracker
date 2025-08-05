6c0f97f965c93753bc7f06e79ffcc7bd1b0c5560
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-08-05 03:42:12 -0600

    Adds activity reducer

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,23 @@
+import type { Activity } from '../types'
+
+export type ActivityActions = 
+{   type: 'save-activity', 
+    payload: { newActivity: Activity }
+}
+
+export type ActivityState = {
+    activities: Activity[]
+}
+
+export const initialState : ActivityState = {
+    activities: []
+}   
+
+export const activityReducer = (
+    state: ActivityState = initialState,
+    action: ActivityActions
+    ) => {
+    if (action.type === 'save-activity') {
+        
+    }
+}
