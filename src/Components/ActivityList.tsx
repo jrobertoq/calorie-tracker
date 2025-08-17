68c940e37e94b131adbef256f3044ae2c4515ac5
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-08-17 10:05:39 -0600

    Adds edit activity functionality

--- src/Components/ActivityList.tsx
+++ src/Components/ActivityList.tsx
@@ -1,6 +1,7 @@
 import type { Activity } from "../types"
 import { categories } from "../data/categories"
 import { useMemo } from "react"
+import { PencilSquareIcon} from '@heroicons/react/24/outline'
 
 type ActivityListProps = {
     activities: Activity[]
@@ -19,7 +20,7 @@ export default function ActivityList({ activities }: ActivityListProps) {
         </h2>
 
         {activities.map(activity => (
-            <div key={activity.id} className='flex justify-betweenbg-white mt-5 border-b border-slate-300 px-5 py-10'>
+            <div key={activity.id} className='flex justify-between bg-white mt-5 border-b border-slate-300 px-5 py-10'>
                 <div className="space-y-2 relative">
                     <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
                         {categoryName(+activity.category)}
@@ -34,8 +35,10 @@ export default function ActivityList({ activities }: ActivityListProps) {
                     </p>
                 </div>
 
-                <div>
-
+                <div className='flex gap-5 items-center'>
+                    <button type="button">
+                        <PencilSquareIcon className='w-8 h-8 text-slate-400' />
+                    </button>
                 </div>
                 
             </div>
